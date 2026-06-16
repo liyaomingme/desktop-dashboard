f83da4577889d4edd991cb552fb9d9595c09ff5d
liyaoming <liyaoming.me@gmail.com>
2026-06-16 08:47:33 +0800

    Update esbuild.config.mjs

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,6 +1,6 @@
 import esbuild from "esbuild";
 import process from "process";
-import builtins from 'builtin-modules';
+import { builtinModules } from "module";
 
 const banner =
 `/*
@@ -11,7 +11,7 @@ if you want to view the source, please visit the github repository of this plugi
 
 const prod = (process.argv[2] === 'production');
 
-esbuild.build({
+const context = await esbuild.context({
     banner: {
         js: banner,
     },
@@ -31,12 +31,19 @@ esbuild.build({
         '@lezer/common',
         '@lezer/highlight',
         '@lezer/lr',
-        ...builtins],
+        ...builtinModules
+    ],
     format: 'cjs',
-    watch: !prod,
     target: 'es2018',
     logLevel: "info",
     sourcemap: prod ? false : 'inline',
     treeShaking: true,
     outfile: 'main.js',
-}).catch(() => process.exit(1));
+});
+
+if (prod) {
+    await context.rebuild();
+    process.exit(0);
+} else {
+    await context.watch();
+}
